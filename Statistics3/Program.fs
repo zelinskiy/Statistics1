113d3c48cdefb2190b39626ac65e2d87235e8b8e
zelinskiy <zelinskiy1917@gmail.com>
2016-12-04 16:05:31 +0200

    fixes

--- Statistics3/Program.fs
+++ Statistics3/Program.fs
@@ -15,6 +15,9 @@ type ChartForm( title, xtitle, ytitle, ptype:SeriesChartType, xs : (float*float)
     do area.AxisX.Title <- xtitle
     do area.AxisY.Title <- ytitle
 
+    //do series.Points.AddXY(1.0, Seq.maxBy (fun (x, y) -> y) xs |> snd ) |> ignore
+    //do series.Points.AddXY(0.0, Seq.maxBy (fun (x, y) -> y) xs |> snd) |> ignore
+
     do chart.Series.Add( series )
     do chart.ChartAreas.Add(area)
     do base.Controls.Add( chart )
@@ -26,7 +29,7 @@ let R = (new Random()).NextDouble
 let count step (xs:float list) = 
     let min = List.min xs |> Math.Floor
     let max = List.max xs |> Math.Ceiling
-    [min .. step .. max - step] 
+    [min .. step .. max] 
         |> List.map (fun s -> s, List.filter (fun x -> x >= s && x <= s + step) xs |> List.length |> float)
 
 let flattenTuples tuples = List.collect (fun (a,b) -> [a;b]) tuples 
@@ -35,25 +38,32 @@ let flattenTuples tuples = List.collect (fun (a,b) -> [a;b]) tuples
 let uniformDistribution a b n = [for x in 1 .. n do yield R() * (b - a) + a ]
 
 
-let centralLimit (r:unit -> float) = 
-    ([for x in 1 .. 12 do yield r() ] |> List.sum) - 6.0
+let _centralLimit() = List.sum [for x in 1 .. 12 do yield R() ] - 6.0
 
+let centralLimit n = 
+    [for x in 1 .. n do yield _centralLimit() ] 
+    |> List.map (fun x -> 0.5 + x / 10.0)
 
-let rec _boxMuller (r:unit -> float) = 
-    let x = r() * (if r() > 0.5 then 1.0 else -1.0)
-    let y = r() * (if r() > 0.5 then 1.0 else -1.0)
+let rec _boxMuller() = 
+    let x = R() * (if R() > 0.5 then 1.0 else -1.0)
+    let y = R() * (if R() > 0.5 then 1.0 else -1.0)
     let s = x * x + y * y
-    if s <= 0.0 || s > 1.0 then _boxMuller r else
-    let z0 = x * Math.Sqrt(-2.0 * Math.Log(s) / s)
-    let z1 = y * Math.Sqrt(-2.0 * Math.Log(s) / s)
-    z0, z1
+    if s <= 0.0 || s > 1.0 then _boxMuller() else
+    let t = Math.Sqrt(-2.0 * Math.Log(s) / s)
+    x * t, y * t 
 
-let boxMuller n = flattenTuples [for x in 1 .. n / 2 do yield _boxMuller R ]
+let boxMuller n = 
+    [for x in 1 .. n / 2 do yield _boxMuller() ] 
+    |> flattenTuples
+    |> List.map (fun x -> 0.5 + x / 10.0)
 
 
-let exponential n = [for x in 1 .. n / 2 do yield -Math.Log(R()) ]
-
-
+//!!!!!!WTF!!!!!!!
+let exponential n = 
+    uniformDistribution 0.0 1.0 n 
+    |> List.map (fun x -> -Math.Log(x) / 4.0 )
+    |> List.filter (fun x -> x <= 1.0 )
+//!!!!!WTF!!!!!!!!
 
 
 let countPlot step size name func = 
@@ -62,9 +72,9 @@ let countPlot step size name func =
         |> count step
     let f = new ChartForm( 
                     name + " " + step.ToString(), 
-                    "Count",                    
-                    "Value",
-                    SeriesChartType.Line,
+                    "Value",                    
+                    "Count",
+                    SeriesChartType.BoxPlot,
                     data)
     Application.Run(f)
 
@@ -73,8 +83,8 @@ let allPlot step size name func =
     let data = func size |> List.mapi (fun i x -> (x, i |> float))
     let f = new ChartForm( 
                     name + " " + step.ToString(), 
-                    "Index",                    
-                    "Value",
+                    "Value",                    
+                    "Index",
                     SeriesChartType.Point,
                     data)
     Application.Run(f)
@@ -82,9 +92,10 @@ let allPlot step size name func =
 
 [<EntryPoint>]
 let rec main argv = 
-    let size = 1000
-    let step = 0.1
-    countPlot step size "Gaussian" boxMuller
-    //gaussianPlot step size
-
+    let size = 80000
+    let step = 0.01    
+    //printfn "%A" ((List.min (exponential size)))
+    //printfn "%A" ((List.max (exponential size)) )
+    //Console.ReadKey();
+    countPlot step size "Exponential" exponential
     0
